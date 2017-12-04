c187d83e395573cbaeb1cd6df793472248ebbb0e
Mark Simulacrum <mark.simulacrum@gmail.com>
2017-12-03 19:27:54 -0700

    Add day 2

--- src/bin/y2017/main.rs
+++ src/bin/y2017/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(test, feature(test))]
-#![feature(option_filter)]
 #[cfg(test)]
 extern crate test;
 #[macro_use] extern crate advent_of_code;
+extern crate smallvec;
 
-gen!(day01);
+gen!(day01, day02);
