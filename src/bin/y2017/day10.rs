17737d23c28d5e5885260362953104dfdb5012fe
Mark Simulacrum <mark.simulacrum@gmail.com>
2017-12-17 17:57:42 -0700

    Optimize reversal for day 10

--- src/bin/y2017/day10.rs
+++ src/bin/y2017/day10.rs
@@ -1,114 +1,19 @@
 #[cfg(test)]
 use quickcheck::TestResult;
 
-fn add(i: usize, n: usize, constraint: usize) -> usize {
-    let mut tot = i + n;
-    while tot >= constraint {
-        tot -= constraint;
-    }
-    tot
-}
-
-struct Circle(Vec<usize>);
-
-impl Circle {
-    fn reverse_after(&mut self, a: usize, length: usize) {
-        let l = self.0.len();
-        if a + length <= l {
-            self.0[a..(a + length)].reverse();
-        } else {
-            let mut sublist = vec![];
-            let mut i = a;
-            let mut idx_a = 0;
-            while sublist.len() != length {
-                sublist.push(self.0[i]);
-                i += 1;
-                if i == self.0.len() {
-                    i = 0;
-                    idx_a = sublist.len();
-                }
-            }
-            sublist.reverse();
-            let mut i = a;
-            for el in &sublist[0..idx_a] {
-                self.0[i] = *el;
-                i += 1;
-            }
-            let mut i = 0;
-            for el in &sublist[idx_a..] {
-                self.0[i] = *el;
-                i += 1;
-            }
-        }
-    }
-
-    #[cfg_attr(not(test), allow(unused))]
-    fn reverse_range(&mut self, a: usize, b: usize) {
-        if b == a { return; }
-        if b < a {
-            let l = self.0.len();
-            self.reverse_after(a, l - a + b);
-        } else {
-            self.reverse_after(a, b - a);
-        }
-    }
-}
-
-#[quickcheck]
-fn circle_rev_simple(mut v: Vec<usize>, a: usize, b: usize) -> TestResult {
-    if a >= v.len() || b >= v.len() || b < a {
-        return TestResult::discard();
-    }
-    let mut circle = Circle(v.clone());
-    circle.reverse_range(a, b);
-    v[a..b].reverse();
-    TestResult::from_bool(&*circle.0 == &v[..])
-}
-
-#[quickcheck]
-fn circle_rev_any(v: Vec<usize>, a: usize, b: usize) -> TestResult {
-    if a >= v.len() || b >= a + v.len() - 1 {
-        return TestResult::discard();
-    }
-    let mut circle = Circle(v.clone());
-    circle.reverse_range(a, b);
-    circle.reverse_range(a, b);
-    TestResult::from_bool(&*circle.0 == &v[..])
-}
-
 pub fn part1(input: &str) -> usize {
     let input = input.split(",").map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
-    let mut list = Circle((0..256).into_iter().collect::<Vec<usize>>());
+    let mut list = (0..256).into_iter().collect::<Vec<usize>>();
     let mut cur = 0;
     let mut skip = 0;
     run(&mut list, &mut cur, &mut skip, &input);
-    list.0[0] * list.0[1]
-}
-
-fn run(list: &mut Circle, cur: &mut usize, skip: &mut usize, lengths: &[usize]) {
-    for &len in lengths {
-        list.reverse_after(*cur, len);
-        *cur = add(*cur, len + *skip, list.0.len());
-        *skip += 1;
-    }
-}
-
-#[test]
-fn part1_1() {
-    let mut c = Circle(vec![0, 1, 2, 3, 4]);
-    run(&mut c, &mut 0, &mut 0, &[3, 4, 1, 5]);
-    assert_eq!(c.0[0] * c.0[1], 12);
-}
-
-#[test]
-fn part1_actual() {
-    assert_eq!(part1(INPUT), 7888);
+    list[0] * list[1]
 }
 
 pub fn part2(input: &str) -> String {
     let mut input = input.as_bytes().iter().map(|x| *x as usize).collect::<Vec<usize>>();
     input.extend_from_slice(&[17, 31, 73, 47, 23]);
-    let mut list = Circle((0..256).into_iter().collect::<Vec<usize>>());
+    let mut list = (0..256).into_iter().collect::<Vec<usize>>();
     let mut cur = 0;
     let mut skip = 0;
     for _ in 0..64 {
@@ -117,13 +22,95 @@ pub fn part2(input: &str) -> String {
     let mut dense = String::new();
     for i in 0..16 {
         let base = i * 16;
-        let mut o = list.0[base];
+        let mut o = list[base];
         for m in 1..16 {
-            o ^= list.0[base + m];
+            o ^= list[base + m];
         }
         dense.push_str(&format!("{:02x}", o));
     }
     dense
+}
+
+#[inline(always)]
+fn add(i: usize, n: usize, constraint: usize) -> usize {
+    if constraint == 0 { return 0; }
+    let mut tot = i + n;
+    while tot >= constraint {
+        tot -= constraint;
+    }
+    tot
+}
+
+fn reverse_after<T>(slice: &mut [T], a: usize, length: usize) {
+    if a + length <= slice.len() {
+        slice[a..(a + length)].reverse();
+    } else {
+        let mut idx = 0;
+        let mut from = a;
+        let mut to = length - (slice.len() - a) - 1;
+        while idx < (length / 2) {
+            slice.swap(from, to);
+            from += 1;
+            if from == slice.len() {
+                from = 0;
+            }
+            if to == 0 {
+                to = slice.len() - 1;
+            } else {
+                to -= 1;
+            }
+            idx += 1;
+        }
+    }
+}
+
+#[quickcheck]
+fn circle_rev_any(v: Vec<usize>, a: usize, length: usize) -> TestResult {
+    if a >= v.len() || length > v.len() {
+        return TestResult::discard();
+    }
+    let mut circle = v.clone();
+    reverse_after(&mut circle, a, length);
+    reverse_after(&mut circle, a, length);
+    TestResult::from_bool(circle == v)
+}
+
+#[test]
+fn circle_rev_1() {
+    let mut v = vec![0, 1, 2, 3, 4, 5];
+    let mut circle = v.clone();
+    let l = circle.len();
+    reverse_after(&mut circle, 0, l);
+    v.reverse();
+    assert_eq!(circle, v);
+}
+
+#[test]
+fn circle_rev_2() {
+    let v = vec![0, 1, 2, 3, 4, 5, 6];
+    let mut circle = v.clone();
+    reverse_after(&mut circle, 5, 5);
+    assert_eq!(circle, &[0, 6, 5, 3, 4, 2, 1]);
+}
+
+fn run(list: &mut [usize], cur: &mut usize, skip: &mut usize, lengths: &[usize]) {
+    for &len in lengths {
+        reverse_after(list, *cur, len);
+        *cur = add(*cur, len + *skip, list.len());
+        *skip += 1;
+    }
+}
+
+#[test]
+fn part1_1() {
+    let mut c = vec![0, 1, 2, 3, 4];
+    run(&mut c, &mut 0, &mut 0, &[3, 4, 1, 5]);
+    assert_eq!(c[0] * c[1], 12);
+}
+
+#[test]
+fn part1_actual() {
+    assert_eq!(part1(INPUT), 7888);
 }
 
 #[test]
