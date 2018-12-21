1152eabd27a898f9aa32bb96c0599bc2954da9a7
Mark Rousskov <mark.simulacrum@gmail.com>
2018-12-20 19:36:46 -0700

    Add link to google spreadsheet for day 10
    This seemed like the best solution even if it is terrible and doesn't
    use code.

---
+++ y2018/src/day10.rs
@@ -0,0 +1,28 @@
+use aoc_macro::{generator, solution};
+
+type Out<'a> = &'a str;
+
+#[generator]
+fn generator(input: &str) -> Out {
+    eprintln!("This day is not implemented in Rust due to requirement of -- essentially -- OCR");
+    eprintln!("See https://docs.google.com/spreadsheets/d/1whrmqDXiUvX2bXp4qoyZMX68JQ0IzY5JAa3yIuphpg8/edit");
+    std::process::exit(1);
+}
+
+#[solution(part1,
+    example_input = generator(EXAMPLE),
+    example = 0)]
+fn part1(input: Out) -> u32 {
+    0
+}
+
+
+#[solution(part2,
+    example_input = generator(EXAMPLE),
+    example = 0)]
+fn part2(_input: Out) -> u32 {
+    0
+}
+
+static EXAMPLE: &str = "";
+static INPUT: &str = "";
